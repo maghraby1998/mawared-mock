f93c2ace974fb93a8db63d0fa536ac0baf3a5128
maghraby1998 <maghraby1998@gmail.com>
2023-07-06 06:32:35 +0300

    added exception factory to validation pipe to parse validation errors as an object instead of an array

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { HttpException, HttpStatus, ValidationPipe } from '@nestjs/common';
+import { ValidationError } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        let validations: any = {};
+        errors.forEach((el: any) => {
+          validations[el.property] = Object.values(el.constraints);
+        });
+
+        return new HttpException(validations, HttpStatus.BAD_REQUEST);
+      },
     }),
   );
   app.enableCors();
