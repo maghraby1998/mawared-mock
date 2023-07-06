a549cb4239bbda67821020314900db685f3ac441
maghraby1998 <maghraby1998@gmail.com>
2023-07-06 05:49:29 +0300

    created seed.ts file

---
+++ prisma/seed.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { UserTypeEnum } from '../src/enums/enums';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const userTypes = await prisma.userType.createMany({
+    data: [
+      {
+        id: 1,
+        name: 'super',
+      },
+      {
+        id: 2,
+        name: 'owner',
+      },
+      {
+        id: 3,
+        name: 'employee',
+      },
+    ],
+  });
+  const users = await prisma.user.createMany({
+    data: [
+      {
+        name: 'super',
+        email: 'super@super.com',
+        password: await bcrypt.hash('123456', 10),
+        userTypeId: UserTypeEnum.SUPER,
+      },
+    ],
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+
+  .catch(async (e) => {
+    console.error(e);
+
+    await prisma.$disconnect();
+
+    process.exit(1);
+  });
